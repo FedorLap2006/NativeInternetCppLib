3cd245027c4c7e02f9a07fd79fe777913c9a6987
FedorLap2006 <fe.lap.prog@gmail.com>
2018-11-15 22:23:54 +0300

    Update NativeIClib.h
    Edit Native Cpp Inet lib files
    Create two classes(child of Inet)

--- NativeIClib.h
+++ NativeIClib.h
@@ -2,22 +2,53 @@
 
 #include <winSock2.h>
 #include <windows.h>
+#include <string>
+
+using namespace std;
 
 #define T_SERVER 1
 #define T_CLIENT 2
 
+#define DEF_NOUSED_PORT 585
+
 typedef long int lint;
 typedef unsigned int uint;
 typedef long unsigned int luint;
+typedef char* lpstrI;
+typedef SOCKET HSOCK;
 
+bool StartupLib();
 
 class Inet{
-	private:
+	protected:
 		lint IP;
 		lint PORT;
-		lint TYPE;
-		SOCKET mainSock;
+		HSOCK mainSock;
+		int protocol;
+		bool bootcheck;
 	public:
-		
+		bool SendM(size_t countinfo,lpstrI info);
+		lpstrI RecvM(size_t countinfo,bool &CRecv);
 };
+
+class Server : public Inet{
+	private:
+		HSOCK recvsock;
+		SOCKADDR_IN serverAddr;
+	public:
+		bool errorinit=false;
+		Server(lint IP,lint PORT=DEF_NOUSED_PORT,lint TypeSock);
+		bool ConnectS();
+		void DisConnectS();
+};
+
+class Client : public Inet{
+	private:
+		SOCKADDR_IN serverSockAddr;
+	public:
+		Client(lint IP,lint PORT=DEF_NOUSED_PORT,lint TypeSock);
+		bool ConnectC();
+		void DisConnectC();
+};
+
 
