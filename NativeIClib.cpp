2013367a447a807e872a0b63244d664233a9b5ad
FedorLap2006 <fe.lap.prog@gmail.com>
2018-12-12 01:16:52 +0300

    Init Relase classes(Client,Server) methods

--- NativeIClib.cpp
+++ NativeIClib.cpp
@@ -1,89 +1,42 @@
 #include "NativeIClib.h"
 
-
-bool StartupLib(){
+Client::Client(string ip,int port){
 	WSADATA wsadata;
 
 	WORD libver=MAKEWORD(2,1);
 	
 	if(WSAStartup(libver,&wsadata) != 0)
 	{
-		return false;
+		isAvaible=false;
 	}
-	return true;
+	else {
+		isAvaible=true;
+
+		saddr.sin_family=AF_INET;
+		saddr.sin_port=port;
+		saddr.sin_addr=inet_addr(ip.c_str());
+	}
+
 }
 
+Server::Server(string ip,int port,bool threads){
+	WSADATA wsadata;
 
-// Inet Class
-
-bool Inet::SendM(SOCKET lsock,size_t countinfo,lpstrI info){
-	HSOCK asock;
-	size_t cbyte=send(lsock,lpstrI,countinfo,0);
-	if(countinfo <=0) return NULL;
-	if(cbyte < countinfo) return false;
-	return true;
-}
-
-lpstrI Inet::RecvM(SOCKET lsock,size_t countinfo,bool &CRecv){
-	lpstrI buff;
-	if(countinfo <=0) return NULL;
-	size_t cread=recv(lsock,buff,countinfo,0);
-	if(cread < countinfo) {CRecv=false;return NULL;}
-	else CRecv=true;
-	return buff;
-}
-
-// Server
-
-
-Server::Server(lpstrI IP,lint PORT,lint TypeSock){
-	this->serverAddr.sin_family=AF_INET;
-	this->serverAddr.sa_data=inet_addr(IP);
-	this->serverAddr.sin_port=PORT;
+	WORD libver=MAKEWORD(2,1);
 	
-	this->mainSock=socket(AF_INET,TypeSock,protocol);
-	bootcheck=false;
-}
-
-bool Server::ConnectS(){
-	if(bootcheck) return true;
-	if(bind(mainSock,(SOCKADDR*)&serverAddr,sizeof(serverAddr))!=0){
-		errorinit=true;
-		return false;
+	if(WSAStartup(libver,&wsadata) != 0)
+	{
+		isAvaible=false;
 	}
-	recvsock=accept(mainSock,&serverAddr,sizeof(serverAddr));
-	bootcheck=true;
-	return true;
-}
+	else {
+		isAvaible=true;
 
-void Server::DisConnectS(){
-	closesocket(recvsock);
-	if(!bootcheck) return;
-	closesocket(mainSock);
-}
-
-Client::Client(lint IP,lint PORT,lint TypeSock){
-	this->serverSockAddr.sin_family=AF_INET;
-	this->serverSockAddr.sa_data=inet_addr(IP);
-	this->serverSockAddr.sin_port=PORT;
-	
-	this->mainSock=socket(AF_INET,TypeSock,protocol);
-	bootcheck=false;
-}
-
-bool Client::ConnectC(){
-	if(bootcheck) return true;
-	if(connect(mainSock,(SOCKADDR*)&serverSockAddr,sizeof(serverAddr))!=0){
-		errorinit=true;
-		return false;
+		sbindaddr.sin_family=AF_INET;
+		sbindaddr.sin_port=port;
+		sbindaddr.sin_addr=inet_addr(ip.c_str());
 	}
-	bootcheck=true;
-	return true;
-}
 
-void Server::DisConnectS(){
-	if(!bootcheck) return;
-	closesocket(mainSock);
+	if(bind(mainSock,sbindaddr,sizeof(sbindaddr)) < 0) isAvaible=false;
+	if(threads) { /* do work */}
 }
-
 
