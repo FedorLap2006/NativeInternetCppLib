3554d9d5c6bc9f71f4ee73ceab1a8849ba3b9ccb
FedorLap2006 <fe.lap.prog@gmail.com>
2018-12-12 13:24:54 +0300

    Add Recv and Send func to Server class

--- NativeIClib.cpp
+++ NativeIClib.cpp
@@ -29,14 +29,42 @@ Server::Server(string ip,int port,bool threads){
 		isAvaible=false;
 	}
 	else {
+		mainSock=socket(AF_INET, SOCK_STREAM, 0);
 		isAvaible=true;
 
 		sbindaddr.sin_family=AF_INET;
 		sbindaddr.sin_port=port;
 		sbindaddr.sin_addr=inet_addr(ip.c_str());
+
 	}
 
 	if(bind(mainSock,sbindaddr,sizeof(sbindaddr)) < 0) isAvaible=false;
 	if(threads) { /* do work */}
+}
+
+int Inet::Send(string &sender){
+	if(!isAvaible) return -1;
+	return send(mainSock,sender.c_str(),sender.size(),0);
+}
+int Inet::Recv(string &reader,size_t size){
+	if(!isAvaible) return -1;
+	char *buffer = new char[size];
+	int b = recv(mainSock,buffer,size,0);
+	reader=buffer;
+	delete[] buffer;
+	return b;
+}
+
+int Recv(string &reader,size_t size){
+	if(!readInitcheck) ReadInit();
+	if(!isAvaible) return -1;
+	char *buffer = new char[size];
+	int b = recv(haccept,buffer,size,0);
+	reader=buffer;
+	delete[] buffer;
+
+	closeSocket(haccept);
+	readInitcheck=false;
+	return b;
 }
 
